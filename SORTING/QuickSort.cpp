cc615170d3db4f8f0282adba2245e5d52de5ac65
the-sadanand <thesadanand.toponed0@gmail.com>
2025-05-26 13:26:07 +0530

    correction to quick sort

--- SORTING/QuickSort.cpp
+++ SORTING/QuickSort.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
 int partition(int arr[] ,int si , int ei){
-    int pivotElement =arr[si];
+    int pivotElement =arr[(si+ei)/2];
     int count=0;
-    for(int i=si+1;i<=ei;i++){
+    for(int i=si;i<=ei;i++){
+        if(i==(si+ei)/2) continue;
         if(arr[i]<=pivotElement)count++;
     }
     int pivotIdx= count +si;
-    swap(arr[si],arr[pivotIdx]);
+    swap(arr[(si+ei)/2],arr[pivotIdx]);
     int i=si;
     int j=ei;
     while(i<pivotIdx && j>pivotIdx){
