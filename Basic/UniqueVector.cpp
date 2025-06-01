40c85d373b59c65cf25e808edd1748ca28351a66
the-sadanand <thesadanand.toponed0@gmail.com>
2025-06-01 21:28:29 +0530

    making unique elements contian form a sorted vector :

--- Basic/UniqueVector.cpp
+++ Basic/UniqueVector.cpp
@@ -2,5 +2,14 @@
 #include<vector>
 using namespace std;
 int main(){
-    
+    vector<int> v ={1,1,3,4,5,6,7,7,7,8,8};
+    vector<int>ans;
+    ans.push_back(v[0]);
+    for(int i=1 ;i<v.size() ; ++i){
+        if(v[i]!=v[i-1]) ans.push_back(v[i]);
+
+    }
+    for(int i=0;i<ans.size();++i){
+        cout<<ans[i]<<" ";
+    }
 }
